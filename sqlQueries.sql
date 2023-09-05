a675dca82b0d53bbb63fad33b4280b1765a45de9
Michelle <mzou1223@gmail.com>
2023-09-05 16:30:20 -0400

    fixing SQL queries

--- sqlQueries.sql
+++ sqlQueries.sql
@@ -1,25 +1,26 @@
 -- SQL Queries:
 -- How many organizations do not have account plans?
 SELECT COUNT(*) 
-FROM organizationorm
-WHERE id NOT IN(SELECT organizationId FROM accountplanorm)
+FROM organization
+WHERE id NOT IN(SELECT organizationId FROM account)
 
 -- How many organizations have more than one account plan?
-SELECT COUNT(*)
-FROM organizationorm
-WHERE organizationID IN(
-    SELECT organizationID 
-    FROM accountplanorm
-    GROUP BY organizationID
+SELECT COUNT(DISTINCT organization.id)
+FROM organization
+WHERE organization.id IN(
+    SELECT account.organizationId 
+    FROM account
+    GROUP BY account.organizationId
     HAVING COUNT(*) > 1)
 
 -- List all organizations that have only one account plan.
-SELECT organizationID 
-FROM accountplanorm
-GROUP BY organizationID
+SELECT account.organizationId, organization.orgName 
+FROM account
+JOIN organization ON account.organizationId = organization.id
+GROUP BY account.organizationId, organization.orgName
 HAVING COUNT(*) = 1
 
 -- List all organizations that have the PASSWORDLESS feature set to true.
-SELECT organizationID
-FROM accountplanorm
-WHERE features ->> "PASSWORDLESS" = "true"
+SELECT organizationId
+FROM account
+
